9920ec5cc8db462763e4792bccbcdd338474d970
emily <snri.atomoxetine@proton.me>
2024-06-05 12:11:30 -0300

    added "powered by mathjs" notice

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,7 +1,15 @@
 import { Component } from "solid-js";
 
 const Layout: Component = (props: any) => {
-  return (
+  return (<>
+    <h1 class="p-3 font-bold text-xl">
+      Powered by&nbsp;
+      <a
+        class="underline text-blue-400 hover:text-blue-500 visited:text-purple-400"
+        href="https://mathjs.org/docs/index.html">
+        math.js
+      </a>
+    </h1>
     <div class="flex flex-col-reverse gap-3 md:flex-row justify-center mt-3 px-3">
       <div class="flex flex-row md:flex-col flex-wrap gap-3 p-3 bg-slate-700 text-black w-full md:w-[21%] md:ms-2 h-fit">
         <Entry href="/">Text Calculator</Entry>
@@ -11,7 +19,7 @@ const Layout: Component = (props: any) => {
         {props.children}
       </div>
     </div>
-  )
+  </>)
 }
 
 interface EntryProps {
